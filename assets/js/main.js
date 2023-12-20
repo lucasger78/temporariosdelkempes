17ab9c929e95c5b6776f53a745d28d0f15ddb86c
Lucas Conti <lucasger78@hotmail.com>
2023-12-20 19:39:10 -0300

    actualización

--- assets/js/main.js
+++ assets/js/main.js
@@ -312,65 +312,109 @@ checkScroll();
 //   });
 // });
 
+// document.addEventListener('DOMContentLoaded', function () {
+//   const formulario = document.querySelector('form');
+
+//   formulario.addEventListener('submit', function (event) {
+//       event.preventDefault(); // Evitar que el formulario se envíe normalmente
+
+//       // Lógica de validación del formulario
+//       const nombre = formulario.querySelector('input[placeholder="Nombre"]').value;
+//       const email = formulario.querySelector('input[placeholder="E-mail"]').value;
+//       const telefono = formulario.querySelector('input[placeholder="Teléfono"]').value;
+//       const asunto = formulario.querySelector('input[placeholder="Asunto"]').value;
+//       const mensaje = formulario.querySelector('textarea').value;
+
+//       if (!nombre || !email || !telefono || !asunto || !mensaje) {
+//           // Si algún campo está vacío, muestra un mensaje de error
+//           Swal.fire({
+//               title: 'Error',
+//               text: 'Por favor, completa todos los campos del formulario.',
+//               icon: 'error',
+//               confirmButtonText: 'OK'
+//           });
+//       } else {
+//           // Envía el formulario utilizando la API Fetch
+//           fetch(formulario.action, {
+//               method: formulario.method,
+//               body: new FormData(formulario)
+//           })
+//           .then(response => {
+//               if (response.ok) {
+//                   // Si el formulario se envía con éxito, reinicia el formulario y luego muestra el SweetAlert
+//                   formulario.reset();
+//                   Swal.fire({
+//                       title: 'TEMPORARIOS DEL KEMPES',
+//                       text: 'Mensaje Enviado ¡Muchas Gracias!',  
+//                       icon: 'success',
+//                       confirmButtonText: 'OK',
+//                       confirmButtonColor: '#e82d2d',
+//                       customClass: {                            
+//                           content: 'swal2-text',
+//                           confirmButton: 'swal2-text2'  
+//                       }
+//                   })
+//                   .then(() => {
+//                       // Después de hacer clic en OK, recarga la página
+//                       window.location.reload();
+//                   });
+//               } else {
+//                   // Si hay un error en el envío, muestra un mensaje de error
+//                   Swal.fire({
+//                       title: 'Error',
+//                       text: 'Hubo un problema al enviar el formulario. Inténtalo de nuevo más tarde.',
+//                       icon: 'error',
+//                       confirmButtonText: 'OK'
+//                   });
+//               }
+//           })
+//           .catch(error => {
+//               console.error('Error en la solicitud fetch:', error);
+//           });
+//       }
+//   });
+// });
+
 document.addEventListener('DOMContentLoaded', function () {
   const formulario = document.querySelector('form');
 
   formulario.addEventListener('submit', function (event) {
-      event.preventDefault(); // Evitar que el formulario se envíe normalmente
+      event.preventDefault();
 
-      // Lógica de validación del formulario
-      const nombre = formulario.querySelector('input[placeholder="Nombre"]').value;
-      const email = formulario.querySelector('input[placeholder="E-mail"]').value;
-      const telefono = formulario.querySelector('input[placeholder="Teléfono"]').value;
-      const asunto = formulario.querySelector('input[placeholder="Asunto"]').value;
-      const mensaje = formulario.querySelector('textarea').value;
+      const nombre = formulario.querySelector('[name="Nombre"]').value;
+      const email = formulario.querySelector('[name="E-mail"]').value;
+      const telefono = formulario.querySelector('[name="Teléfono"]').value;
+      const asunto = formulario.querySelector('[name="Asunto"]').value;
+      const mensaje = formulario.querySelector('[name="Mensaje"]').value;
 
       if (!nombre || !email || !telefono || !asunto || !mensaje) {
-          // Si algún campo está vacío, muestra un mensaje de error
-          Swal.fire({
-              title: 'Error',
-              text: 'Por favor, completa todos los campos del formulario.',
-              icon: 'error',
-              confirmButtonText: 'OK'
-          });
+          mostrarError('Por favor, completa todos los campos del formulario.');
       } else {
-          // Envía el formulario utilizando la API Fetch
-          fetch(formulario.action, {
-              method: formulario.method,
-              body: new FormData(formulario)
-          })
-          .then(response => {
-              if (response.ok) {
-                  // Si el formulario se envía con éxito, reinicia el formulario y luego muestra el SweetAlert
-                  formulario.reset();
-                  Swal.fire({
-                      title: 'TEMPORARIOS DEL KEMPES',
-                      text: 'Mensaje Enviado ¡Muchas Gracias!',  
-                      icon: 'success',
-                      confirmButtonText: 'OK',
-                      confirmButtonColor: '#e82d2d',
-                      customClass: {                            
-                          content: 'swal2-text',
-                          confirmButton: 'swal2-text2'  
-                      }
-                  })
-                  .then(() => {
-                      // Después de hacer clic en OK, recarga la página
-                      window.location.reload();
-                  });
-              } else {
-                  // Si hay un error en el envío, muestra un mensaje de error
-                  Swal.fire({
-                      title: 'Error',
-                      text: 'Hubo un problema al enviar el formulario. Inténtalo de nuevo más tarde.',
-                      icon: 'error',
-                      confirmButtonText: 'OK'
-                  });
-              }
-          })
-          .catch(error => {
-              console.error('Error en la solicitud fetch:', error);
-          });
+          mostrarExito('¡Mensaje Enviado! ¡Muchas Gracias!');
+          formulario.reset();
       }
   });
+
+  function mostrarError(mensaje) {
+      Swal.fire({
+          title: 'Error',
+          text: mensaje,
+          icon: 'error',
+          confirmButtonText: 'OK',
+      });
+  }
+
+  function mostrarExito(mensaje) {
+      Swal.fire({
+          title: 'TEMPORARIOS DEL KEMPES',
+          text: mensaje,
+          icon: 'success',
+          confirmButtonText: 'OK',
+          confirmButtonColor: '#e82d2d',
+          customClass: {
+              content: 'swal2-text',
+              confirmButton: 'swal2-text2',
+          },
+      });
+  }
 });
